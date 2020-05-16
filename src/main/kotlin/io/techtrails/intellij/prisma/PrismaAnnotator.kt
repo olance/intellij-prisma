ff594266563f9bbae2bc747280b6ad064135991a
Olivier Lance <olivier@techtrails.io>
2020-05-16 12:14:48 +0200

    Adds Annotator for PSI-based highlighting

---
+++ src/main/kotlin/io/techtrails/intellij/prisma/PrismaAnnotator.kt
@@ -0,0 +1,26 @@
+package io.techtrails.intellij.prisma
+
+import com.intellij.lang.annotation.AnnotationHolder
+import com.intellij.lang.annotation.Annotator
+import com.intellij.lang.annotation.HighlightSeverity.INFORMATION
+import com.intellij.openapi.editor.colors.TextAttributesKey
+import com.intellij.psi.PsiElement
+import io.techtrails.intellij.prisma.psi.*
+import io.techtrails.intellij.prisma.PrismaSyntaxHighlighter.Companion as Highlight
+
+class PrismaAnnotator : Annotator {
+    override fun annotate(element: PsiElement, holder: AnnotationHolder) {
+        when(element) {
+            is PrismaBlockName          -> holder.annotateSyntax(element, Highlight.BLOCK_NAME)
+            is PrismaTypeName           -> holder.annotateSyntax(element, Highlight.TYPE_NAME)
+            is PrismaFieldName          -> holder.annotateSyntax(element, Highlight.FIELD_NAME)
+            is PrismaTypeModifier       -> holder.annotateSyntax(element, Highlight.TYPE_MODIFIER)
+            is PrismaAttributeArgName   -> holder.annotateSyntax(element, Highlight.ATTRIBUTE_ARG_NAME)
+            is PrismaEnumConstant       -> holder.annotateSyntax(element, Highlight.ENUM_CONSTANT)
+        }
+    }
+
+    private fun AnnotationHolder.annotateSyntax(element: PsiElement, attr: TextAttributesKey) {
+        newSilentAnnotation(INFORMATION).range(element).textAttributes(attr).create()
+    }
+}
